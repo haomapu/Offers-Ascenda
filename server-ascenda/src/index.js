619ceb451440cb246ff1d63e492b4b92c0a21ca9
haomapu <haoanh7702@gmail.com>
2023-01-16 17:44:00 +0700

    Update readme and remove consolelog

--- server-ascenda/src/index.js
+++ server-ascenda/src/index.js
@@ -1,30 +1,14 @@
 import express from 'express';
-import { engine } from 'express-handlebars';
 import offerRoute from './routes/offer.route.js'
-import { fileURLToPath } from "url";
 import cors from 'cors'
 import bodyParser from "body-parser";
-import { dirname } from "path";
 
 const app = express()
 const PORT = "8080";
-const __dirname = dirname(fileURLToPath(import.meta.url));
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.engine(
-  "hbs",
-  engine({
-    defaultLayout: "main.hbs",
-    helpers: {
-    },
-  })
-);
-app.set('view engine', 'handlebars');
-app.set("view engine", "hbs");
-app.set("views", __dirname + "/views");
-
-
 
 app.use('/', offerRoute);
 
--- server-ascenda/src/routes/offer.route.js
+++ server-ascenda/src/routes/offer.route.js
@@ -3,9 +3,6 @@ import offerController from "../controllers/offer.controller.js";
 
 const router = express.Router();
 
-router.get("/", offerController.getAllOffer);
-
-router.post("/", offerController.getFilterOffer);
 
 router.post("/api/filter", offerController.apiFilter);
 
